4a11f1360a42954eacc42b97457aa6e69fc7ee42
Scorp <sf_commander@outlook.com>
2023-06-01 23:48:36 +0300

    TO-DO list bitirildi.

--- content/todo-list/main.js
+++ content/todo-list/main.js
@@ -1,48 +1,139 @@
 // variables
 const todoInput = document.querySelector(".form-input");
 const todoForm = document.querySelector(".form");
-const searchInput = document.querySelector(".form-input");
+const searchInput = document.querySelector(".look-input");
 const todoOutput = document.querySelector(".output");
 
 
 //functions
 const addTodo = (e) => {
     e.preventDefault();
-    const todo = todoInput.ariaValueMax;
+    const todo = todoInput.value;
     if(todo){
         const id = Date.now();
         addTodoToLocalStorage({id, todo, completed: false});
         const todoItem = document.createElement("div");
         todoItem.className = `item ${id}`;
-        todoItem.innerHTML `
+        todoItem.innerHTML= 
+        `
                 <input type="checkbox" class="check">
                 <input type="text" class="text" value=${todo} disabled>
-                <button class="edit">Edit</button>
-                <button class="delete">Delete</button>
+                <button class="edit">Düzenle</button>
+                <button class="delete">Sil</button>
         `;  
         todoOutput.appendChild(todoItem);
         todoInput.value = "";  
     };
 };
-/*
-const deleteTodo = (e) => {};
 
-const checkedTodo = (e) => {};
+const searchTodo = (e) => {
+    const searchText = e.target.value.toLowerCase();
+    const todoItems = document.querySelectorAll(".item");
+    todoItems.forEach(todoItem => {
+        const todo = todoItem.querySelector(".text").value.toLowerCase();
+        if(todo.indexOf(searchText) !==  -1){
+            todoItem.style.display = "grid";
+        }   else{
+            todoItem.style.display = "none";
+        }
+    })
+};
 
-const editTodo = (e) => {};
+const editTodo = (e) => {
+    if(e.target.className === "edit"){
+        const id = e.target.parentElement.classList[1];
+        const todoText = e.target.parentElement.querySelector(".text");
+        if(todoText.disabled) {
+            todoText.disabled = false; 
+            e.target.textContent = "Kaydet";
+        }
+        else{
+            todoText.disabled = true;
+            e.target.textContent = "Düzenle";
+            editTodoInLocalStorage(id,todoText.value);
+        }
+    }
+};
 
-const getTodo = (e) => {};
+const getTodo = (e) => {
+    let tasks = localStorage.getItem("tasks");
+    if(tasks) {
+        tasks = JSON.parse(tasks);
+        tasks.forEach((task) => {
+            const todoItem = document.createElement("div");
+            todoItem.className = `item ${task.id}`; 
+            todoItem.innerHTML = 
+            ` 
+            <input type="checkbox" class="check" ${task.completed ? "checked" : ""}>
+            <input type="text" class="text ${task.completed ? "hit" : ""}" value=${todo} disabled>
+            <button class="edit">Edit</button>
+            <button class="delete">Delete</button> })}}
+            `;
+            todoOutput.appendChild(todoItem);
+        });
+    }
+};
 
-const searchTodo = (e) => {};
-*/
+const checkedTodo = (e) => {
+    if(e.target.className === "check"){
+        const id = e.target.parentElement.classList[1];
+        const checked = e.target.checked;
+
+        if(checked){
+           e.target.nextElementSibling.style.textDecoration = "line-through";
+        }
+        else{  
+           e.target.nextElementSibling.style.textDecoration = "none";
+       }
+       let tasks = JSON.parse(localStorage.getItem("tasks"));
+       tasks = tasks.map(task => {
+           if(task.id === parseInt(id)){
+               task.completed = checked;
+           }
+           return task;
+       });
+       localStorage.setItem("tasks", JSON.stringify(tasks));
+   }
+};
+
+const deleteTodo = (e) => {
+    if(e.target.className === "delete"){
+        deleteTodoFromLocalStorage(e.target.parentElement.classList[1]);
+        e.target.parentElement.remove();
+    }
+};
+
+const editTodoInLocalStorage = (id, todo) => {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = tasks.map( task => {
+         if(task.id === parseInt(id)){
+            task.todo = todo;
+         }
+         return task;
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+const deleteTodoFromLocalStorage = (id) => {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    tasks = tasks.filter((task) => task.id !== parseInt(id));
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+const addTodoToLocalStorage = (todo) => {
+    let tasks = JSON.parse(localStorage.getItem("tasks"));
+    if(!tasks){
+        tasks = [];
+    }
+    tasks.push(todo);    
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
 
 //eventlisteners
+todoOutput.addEventListener("click", checkedTodo);
 todoForm.addEventListener("submit", addTodo);
-
-//stodoOutput.addEventListener("click", deleteTodo);
-//stodoOutput.addEventListener("click", checkedTodo);
-//stodoOutput.addEventListener("click", editTodo);
-//document.addEventListener("DOMContentLoaded", getTodo);
-//searchInput.addEventListener("keyup", searchTodo);
-
-//<script src="./content/todo-list/main.js"></script>
+document.addEventListener("DOMContentLoaded", getTodo);
+todoOutput.addEventListener("click", deleteTodo);
+todoOutput.addEventListener("click", editTodo);
+searchInput.addEventListener("keyup", searchTodo);
