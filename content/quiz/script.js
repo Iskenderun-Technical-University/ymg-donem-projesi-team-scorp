43d89eb46d9e9fd13ee4cb28c8d7c229169969ac
Baylrden <106672385+Baylrden@users.noreply.github.com>
2023-06-03 16:36:17 +0400

    QUIZ uygulaması güncellendi.
    HTML ve JS ufak değişiklikler yapıldı, CSS kodları yazıldı.

---
+++ content/quiz/script.js
@@ -0,0 +1,39 @@
+const questions = [
+    {
+        question: "2.Dünya Savaşı hangi ülkenin işgaliyle başlamıştır?",
+        answers: [
+            { text: "Vatikan", correct: false},
+            { text: "Endonezya", correct: false},
+            { text: "Polonya", correct: true},
+            { text: "Avusturya", correct: false},
+        ]
+    },
+    {
+        question: "Dünyadaki en küçük ülke hangisidir?",
+        answers: [
+            { text: "Vatikan", correct: true},
+            { text: "Şri Lanka", correct: false},
+            { text: "Peru", correct: false},
+            { text: "Faroe Adaları", correct: false},
+        ]
+    },
+    {
+        question: "Dünyada en çok konuşulan dil hangisidir?",
+        answers: [
+            { text: "İngilizce", correct: false},
+            { text: "Çince", correct: true},
+            { text: "Japonca", correct: false},
+            { text: "Portekizce", correct: false},
+        ]
+    },
+    {
+        question: "insan başına düşen metre kare hesabına göre en kalabalık ülke hangisidir?",
+        answers: [
+            { text: "Çin", correct: false},
+            { text: "Bangladeş", correct: true},
+            { text: "Nepal", correct: false},
+            { text: "Butan", correct: false},
+        ]
+    }
+];
+
