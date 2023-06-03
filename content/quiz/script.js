ff3b5123ef7d86a65f88f730a61b8e229c4a4c79
Scorp <sf_commander@outlook.com>
2023-06-03 16:53:48 +0300

    conflict resolved

--- content/quiz/script.js
+++ content/quiz/script.js
@@ -37,3 +37,35 @@ const questions = [
     }
 ];
 
+
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-button");
+const nextButton = document.getElementById("next-btn"); 
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0; 
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState
+    let currentQuestion = questions[currentQuestionIndex];
+    let question = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButton.appendChild(button);
+    })
+}
+
+startQuiz();
